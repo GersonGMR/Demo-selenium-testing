317131d8d6fa7ed85c8ebdef49b404d215aaaf29
GersonGMR <56897299+GersonGMR@users.noreply.github.com>
2023-03-14 12:13:27 -0600

    importing project
    all files needed to a simple test structure

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class App {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://www.wikipedia.org");
+    }
+}
