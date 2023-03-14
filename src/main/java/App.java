90f3b43024cb4bbe7695e8f1cafea51403d397d4
GersonGMR <56897299+GersonGMR@users.noreply.github.com>
2023-03-14 12:36:40 -0600

    prueba de tiempo, retrieve
    se obtiene el tiempo en milisegundos que tarda en obtener el resultado, utilizando locators, xpath y cssselector

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,5 +10,26 @@ public class App {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://www.wikipedia.org");
+
+        //locators
+        //id
+        Long start = System.currentTimeMillis();
+        driver.findElement(By.id("js-link-box-en"));
+        Long end = System.currentTimeMillis();
+
+        System.out.println("El tiempo necesario para obtener el locator por id es: " + (end-start));
+
+        //xpath
+        start = System.currentTimeMillis();
+        driver.findElement(By.xpath("//*[@id=\"www-wikipedia-org\"]/div[2]/div[2]"));
+        end = System.currentTimeMillis();
+        System.out.println("El tiempo necesario para obtener el xpath es: " + (end-start));
+        //cssSelector
+        start = System.currentTimeMillis();
+        driver.findElement(By.cssSelector("#www-wikipedia-org > div.central-featured > div.central-featured-lang.lang3"));
+        end = System.currentTimeMillis();
+        System.out.println("El tiempo necesario para obtener el cssSelector es: " + (end-start));
+
+        driver.close();
     }
 }
