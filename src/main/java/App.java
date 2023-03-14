8102647863b20491bbc37a7cde5194813cb72d66
GersonGMR <56897299+GersonGMR@users.noreply.github.com>
2023-03-14 12:49:27 -0600

    Test obtener texto y comparar
    se obtiene el texto contenido en un span, para luego compararlo con el resultado esperado y verificar si el test falla o es correcto

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,12 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -29,6 +30,18 @@ public class App {
         driver.findElement(By.cssSelector("#www-wikipedia-org > div.central-featured > div.central-featured-lang.lang3"));
         end = System.currentTimeMillis();
         System.out.println("El tiempo necesario para obtener el cssSelector es: " + (end-start));
+
+        //obtener texto y compararlo para verificar que el test es correcto
+        WebElement tittleWebPage = driver.findElement(By.cssSelector("#www-wikipedia-org > div.central-textlogo > h1 > span"));
+        String titleText = tittleWebPage.getText();
+        String expectedTitleText = "Wikipedia";
+        if (titleText.equals(expectedTitleText)) {
+            System.out.println("Test has passed.");
+        }else {
+            System.out.println("Test has failed");
+            driver.close();
+            throw new Exception();
+        }
 
         driver.close();
     }
